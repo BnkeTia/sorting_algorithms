8fe8c510868731ed7408f5ac22fb3bb114b1c13e
BnKeTia <bernardnketia51@gmail.com>
2023-11-18 19:19:47 +0000

    restructred bubbly

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,59 +1,39 @@
 #include "sort.h"
 
 /**
- * swap - A function that swaps integers in a sorting algo
- * @x: first integer to be swapped
- * @y: second integer to be swapped
- * Return: void
- */
-void swap(int *x, int *y)
-{
-	int temp = *x;
-
-	*x = *y;
-	*y = temp;
-}
-
-/**
  * bubble_sort - A function that sorts an array of integers using
  * the bubble sort algorithm
  * @array: pointer to the array
- * @size: size of the array of intgegers
+ * @size: size of the array of integers
  * Return: void
  */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int swapped;
+	int isSorted, temp;
 
-	printf("Initial array:\n");
-	print_array(array, size);
-	printf("\n");
+	/* verify if array is empty or has only 2 elements */
+	if (array == NULL || size < 2)
+		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		swapped = 0;
-
+		isSorted = 0;
+		/* iterate through the array */
 		for (j = 0; j < size - i - 1; j++)
 		{
+			/* setting condition to swap elements */
 			if (array[j] > array[j + 1])
 			{
-				swap(&array[j], &array[j + 1]);
-				swapped = 1;
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				/* Print array after each swap */
+				print_array(array, size);
+				isSorted = 1;
 			}
 		}
-		if (swapped)
-		{
-			printf("step %lu:\n", (unsigned long)(i + 1));
-			print_array(array, size);
-			printf("\n");
-		}
-		else
-		{
-			printf("Array is already sorted. Exiting loop.\n");
+		if (isSorted == 0)
 			break;
-		}
 	}
-	printf("Final sorted array:\n");
-	print_array(array, size);
 }
