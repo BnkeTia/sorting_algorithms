056a072f7690812692358a8d6aeea40585ef0d6d
BnKeTia <bernardnketia51@gmail.com>
2023-11-18 18:49:55 +0000

    added the swap feature

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -24,16 +24,36 @@ void swap(int *x, int *y)
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
+	int swapped;
+
+	printf("Initial array:\n");
+	print_array(array, size);
+	printf("\n");
 
 	for (i = 0; i < size - 1; i++)
 	{
+		swapped = 0;
+
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				swap(&array[j], &array[j + 1]);
-				printf("Swap: %d, %d\n", array[j], array[j + 1]);
+				swapped = 1;
 			}
 		}
+		if (swapped)
+		{
+			printf("step %lu:\n", (unsigned long)(i + 1));
+			print_array(array, size);
+			printf("\n");
+		}
+		else
+		{
+			printf("Array is already sorted. Exiting loop.\n");
+			break;
+		}
 	}
+	printf("Final sorted array:\n");
+	print_array(array, size);
 }
